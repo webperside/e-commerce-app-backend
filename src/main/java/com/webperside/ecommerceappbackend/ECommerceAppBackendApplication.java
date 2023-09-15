1bda05d6aa313b1b451e4364e2d1226b3724526c
Hamid Sultanzadeh <hamidsultanzadeh@Hamids-MacBook-Pro.local>
2023-09-15 14:49:15 +0400

    initial

---
+++ src/main/java/com/webperside/ecommerceappbackend/ECommerceAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webperside.ecommerceappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webperside/ecommerceappbackend/ECommerceAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webperside.ecommerceappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
